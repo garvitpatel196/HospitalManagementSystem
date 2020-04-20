0f14a8ba245b4a2b95eca8801cc9521647e0355f
PallaviKotveer <pallavikotvir@gmail.com>
2020-04-20 15:49:23 -0400

    adding shared folder

---
+++ src/shared/CommonVariables.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shared;
+
+/**
+ *
+ * @author NIAR Tech
+ */
+public class CommonVariables {
+      public static String secretKey = "SecretGroup123";
+      public static String salt = "DontShare";
+       public static String instanceName = "PBKDF2WithHmacSHA256";
+      
+       
+       
+}
