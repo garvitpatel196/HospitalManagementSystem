49be832accdf13d4428fbf330fb2d6ad890a8138
Garvit Patel <garvitpatel196@gmail.com>
2020-04-21 19:41:05 -0400

    Updates

--- src/shared/WriteToExcel.java
+++ src/shared/WriteToExcel.java
@@ -4,8 +4,8 @@
  * and open the template in the editor.
  */
 package shared;
+
 import java.io.File;
-import jxl.Cell;
 import jxl.Workbook;
 import jxl.write.Label;
 import jxl.write.WritableSheet;
@@ -16,40 +16,37 @@ import jxl.write.WritableWorkbook;
  * @author NIAR Tech
  */
 public class WriteToExcel {
-    public void writeData(String fileName, String[] labels,String[] data) throws Exception{
-        String path = CommonVariables.directoryPath+fileName;
+
+    public void writeData(String fileName, String[] labels, String[] data) throws Exception {
+        String path = CommonVariables.directoryPath + fileName;
         int noOfFields = labels.length;
         int rowCount = 0;
         File file = new File(path);
         WritableWorkbook wb;
         WritableSheet sht;
-        if(file.exists()){
+        if (file.exists()) {
             Workbook existingWb = Workbook.getWorkbook(new File(file.getAbsolutePath()));
             wb = Workbook.createWorkbook(new File(path), existingWb);
-        }else {
-          wb = Workbook.createWorkbook(file);
-          sht = wb.createSheet("data", 0);
-          for (int i = 0; i<noOfFields; i++){
-              sht.addCell(new Label (i, 0, labels[i]));
-          }
+        } else {
+            wb = Workbook.createWorkbook(file);
+            sht = wb.createSheet("data", 0);
+            for (int i = 0; i < noOfFields; i++) {
+                sht.addCell(new Label(i, 0, labels[i]));
+            }
         }
         sht = wb.getSheet(0);
         rowCount = sht.getRows();
-        
-        for (int colNum = 0; colNum <noOfFields; colNum++){
-        sht.addCell(new Label (colNum,rowCount,data[colNum]));
-        }
-        
-        
 
-  
+        for (int colNum = 0; colNum < noOfFields; colNum++) {
+            sht.addCell(new Label(colNum, rowCount, data[colNum]));
+        }
+
 //  Label ll = new Label(0, 0, "Result");
 //  sht.addCell(ll);
 //  sht.addCell(new Label(1, 1, "Pass"));
 //  sht.addCell(new Label(2, 2, "Fail"));
-  
-  wb.write();
-  wb.close();
-  System.out.println("Workbook is created");
+        wb.write();
+        wb.close();
+        System.out.println("Workbook is created");
     }
 }
